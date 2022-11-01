febf20db8de27e23cde9f61e6212ffe197bb37e0
raster-gen <rasterroot@gmail.com>
2022-11-01 07:33:46 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gennady Raster on 1.11.22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
